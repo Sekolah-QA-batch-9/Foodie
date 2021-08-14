c8d4c6006da86b5ebb8992c0cfc11b8f83a04b3f
sopigp <sopigp@gmail.com>
2021-08-14 12:14:41 +0700

    midtrans fixing koma

---
+++ Test Suites/Android/Apk_Calculator.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Apk_Calculator</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d13a74e6-16f0-40a2-9b8e-fa07b9ba1e80</testSuiteGuid>
+   <testCaseLink>
+      <guid>e2bf1196-6a95-4fcb-bcc5-63a114e6e6cf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Android/Calculator/Start_Apk_Android_Calculator</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>35e816ef-a4c1-4d43-aac7-8d9df399e835</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Android/Calculator/Pertambahan_Calculator</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8db8b91d-cd70-4aab-968b-1926ec84255e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Android/Calculator/Perkalian_Calculator</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>eef11345-a4b6-42ec-a441-c950c19da503</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Android/Calculator/Pengurangan_Calculator</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5de57182-007b-4932-8671-9116789a6cdc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Android/Calculator/Pembagian_Calculator</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Android/Apk_Foody.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Apk_Foody</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d9068771-e402-4424-8240-d418c10f404c</testSuiteGuid>
+   <testCaseLink>
+      <guid>28998301-d030-42d4-94d2-c9dcbe3f52df</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Android/Foody/TC1_LoginFacebook</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>bccaeb47-ab07-4c16-90b7-fda113658040</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7edb0291-8e6a-4527-b495-651e8e3d78b5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Android/Foody/TC2_MencariMakanan</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>c281b5ae-56cf-4e12-807e-fcb7dd9447bf</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>19e711e0-7c8e-4423-8f30-94c04a6737fc</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>622ceac4-60b5-40e3-8a87-ca61baf02e55</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Android/Foody/TC3_Review_Makanan</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8a6b55b7-f867-450d-873d-d2fdfbe88742</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Android/Foody/TC3_Logout</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
